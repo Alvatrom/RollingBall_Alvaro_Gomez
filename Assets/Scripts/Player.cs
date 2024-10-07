e66e032004fa375e2e13490331db65b4be561b8e
Alvatrom <alvarogomez0402@gmail.com>
2024-10-07 20:22:33 +0200

    Impulsos en pelota
    recordar hacer mas pequeña la pelota para que el movimiento sea mas fluido

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] Vector3 direccion;
+    [SerializeField] float fuerza = 5;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
+        GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(direccion*fuerza,ForceMode.Impulse);
+        }
+
         
     }
 }
