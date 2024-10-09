bd2b313fe0dd19ba3065d7fa10e5314ed4fa61f3
Alvatrom <alvarogomez0402@gmail.com>
2024-10-09 17:13:33 +0200

    Canvas y movimiento

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] Vector3 direccion;
-    [SerializeField] float fuerza = 5;
+    [SerializeField] float fuerza = 5, fuerzaSalto = 8f;
     Rigidbody rb;
+    float h, v;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,15 +16,24 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         
     }
-
+    
     // Update is called once per frame
     void Update()
     {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(direccion*fuerza,ForceMode.Impulse);
+            //rb.AddForce(new Vector3(0, 1, 0).normalized * fuerzaSalto, ForceMode.Force);
         }
 
+        
+    }
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(-v, 0, h).normalized * fuerza, ForceMode.Force);
         
     }
 }
